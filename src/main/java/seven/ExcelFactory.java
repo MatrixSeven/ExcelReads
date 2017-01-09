8ca2cf49f8f347167c14031d3368f361a12a8d57
MatrixSeven <hacker.kill07@gmail.com>
2017-01-09 14:07:09 +0800

    增加方法，完善异常体系，详见更新日志

--- src/main/java/seven/ExcelFactory.java
+++ src/main/java/seven/ExcelFactory.java
@@ -39,9 +39,24 @@ public class ExcelFactory {
 	private ExcelFactory() {
 	}
 
+    /**
+     * 读取Excel
+     * @param FilePath  路径
+     * @param r 包装类
+     * @return
+     * @throws Exception
+     */
 	public static Wrapper getBeans(String FilePath, WrapperObj r) throws Exception {
 		return (Wrapper)r.init(FilePath);
 	}
+
+    /**
+     * 保存Excel
+     * @param bean
+     * @param FilePath
+     * @return
+     * @throws Exception
+     */
 	public static SaveExcel saveExcel(List<? extends Object> bean, String FilePath) throws Exception {
             if (bean.size() < 1) {
                 throw new Exception("请传入数据");
@@ -51,15 +66,48 @@ public class ExcelFactory {
             }
          return new ResExprotObj((List)bean, FilePath);
 	}
+
+    /**
+     * 保存Excel
+     * @param resultSet
+     * @param FilePath
+     * @return
+     * @throws Exception
+     */
     public static SaveExcel saveExcel(ResultSet resultSet, String FilePath) throws Exception {
             return new ResExprotDBMap(resultSet,FilePath);
     }
+
+    /**
+     * 保存Excel
+     * @param resultSet
+     * @param FilePath
+     * @param packageDataInterface
+     * @return
+     * @throws Exception
+     */
     public static SaveExcel saveExcel(ResultSet resultSet, String FilePath, PackageDataInterface packageDataInterface) throws Exception {
         return new ResExprotDBObj(resultSet,FilePath,packageDataInterface);
     }
+
+    /**
+     * 保存Excel
+     * @param resultSet
+     * @param type
+     * @return
+     * @throws Exception
+     */
     public static SaveExcel saveExcel(ResultSet resultSet,Class type) throws Exception {
         return new ResExprotDBObj(resultSet,type);
     }
+
+
+    /**
+     * 保存Excel
+     * @param resultSet
+     * @return
+     * @throws Exception
+     */
     public static SaveExcel saveExcel(ResultSet resultSet) throws Exception {
         return new ResExprotDBMap(resultSet);
     }
